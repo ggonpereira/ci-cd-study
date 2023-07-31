d8d4bac5f0c1f4ad21f974c7251cc697da40e512
Gabriel Gonçalves Pereira <ggonpereira@gmail.com>
2023-07-31 00:49:09 -0300

    👌 improv: add spinner to button on loading state

--- src/view/components/Button/Button.tsx
+++ src/view/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import { ComponentProps } from "react";
 import { cn } from "../../../app/utils/cn";
+import { Spinner } from "../Spinner";
 
 interface ButtonProps extends ComponentProps<"button"> {
   children: React.ReactNode;
@@ -18,11 +19,13 @@ export const Button = ({
       {...props}
       disabled={disabled || isLoading}
       className={cn(
-        "active:bg h-12 rounded-2xl bg-blue-900 px-6 font-medium tracking-[-0.5px] text-white transition-all hover:bg-blue-800 focus:bg-blue-950 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400",
+        "active:bg flex h-12 items-center justify-center gap-2 rounded-2xl bg-blue-900 px-6 font-medium tracking-[-0.5px] text-white transition-all hover:bg-blue-800 focus:bg-blue-950 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400",
         className,
       )}
     >
-      {children}
+      {!isLoading && children}
+
+      {isLoading && <Spinner className="h-6 w-6" />}
     </button>
   );
 };
