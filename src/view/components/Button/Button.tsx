71c50693cfcbcacb1bb41c5028c61b2ffb82d887
Gabriel Gonçalves Pereira <ggonpereira@gmail.com>
2023-07-31 00:32:41 -0300

    🎉 feat: setup axios, react-query and react-hot-toast / improve button for loading state

--- src/view/components/Button/Button.tsx
+++ src/view/components/Button/Button.tsx
@@ -3,12 +3,20 @@ import { cn } from "../../../app/utils/cn";
 
 interface ButtonProps extends ComponentProps<"button"> {
   children: React.ReactNode;
+  isLoading?: boolean;
 }
 
-export const Button = ({ children, className, ...props }: ButtonProps) => {
+export const Button = ({
+  children,
+  className,
+  isLoading,
+  disabled,
+  ...props
+}: ButtonProps) => {
   return (
     <button
       {...props}
+      disabled={disabled || isLoading}
       className={cn(
         "active:bg h-12 rounded-2xl bg-blue-900 px-6 font-medium tracking-[-0.5px] text-white transition-all hover:bg-blue-800 focus:bg-blue-950 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400",
         className,
