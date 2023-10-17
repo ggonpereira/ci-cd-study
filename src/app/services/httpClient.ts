cdb1c862835ce1b5280203b2bbe7d98bfc933096
Gabriel Gonçalves Pereira <ggonpereira@gmail.com>
2023-10-16 21:22:10 -0300

    🎉 feat: mock api response delay

--- src/app/services/httpClient.ts
+++ src/app/services/httpClient.ts
@@ -2,6 +2,10 @@ import axios, { InternalAxiosRequestConfig } from "axios";
 import { cookieKeys } from "../config/cookieKeys";
 import { getCookie } from "../utils/cookie";
 
+const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
@@ -17,3 +21,8 @@ const reqSuccessInterceptor = async (config: InternalAxiosRequestConfig) => {
 };
 
 httpClient.interceptors.request.use(reqSuccessInterceptor);
+httpClient.interceptors.response.use(async (data) => {
+  await sleep(500);
+
+  return data;
+});
