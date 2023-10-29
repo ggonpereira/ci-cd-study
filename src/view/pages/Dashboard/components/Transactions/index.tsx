fd739fe811606b0c55483b156da48f33522baf75
Gabriel Gonçalves Pereira <ggonpereira@gmail.com>
2023-10-29 19:27:58 -0300

    🎉 feat: implement empty transactions state

--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -1,7 +1,9 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { MONTHS } from "../../../../../app/constants/months";
 import { useWindowWidth } from "../../../../../app/hooks/useWindowWidth";
+import emptyStateImage from "../../../../../assets/empty_state.svg";
 import { Spinner } from "../../../../components/Spinner";
+import { IconType } from "../../../../components/icons/categories/CategoryIcon";
 import { HeaderFilters } from "./HeaderFilters";
 import { SliderNavigation } from "./SliderNavigation";
 import { SliderOption } from "./SliderOption";
@@ -10,61 +12,85 @@ import { useTransactionsController } from "./useTransactionsController";
 
 export const Transactions = () => {
   const width = useWindowWidth();
-  const { isLoading } = useTransactionsController();
+  const { transactions, isInitialLoading, isLoading } =
+    useTransactionsController();
+
+  const hasTransactions = transactions.length > 0;
 
   return (
     <div className="flex h-full w-full flex-col gap-4 rounded-2xl bg-gray-100 p-10 ">
-      {isLoading && (
+      {isInitialLoading && (
         <div className="flex h-full w-full items-center justify-center">
           <Spinner className="h-10 w-10" />
         </div>
       )}
 
-      {!isLoading && (
-        <>
-          <header className="flex flex-col gap-6">
-            <HeaderFilters />
+      {!isInitialLoading && (
+        <header className="flex flex-col gap-6">
+          <HeaderFilters />
 
-            <div className="flex gap-4">
-              <Swiper
-                slidesPerView={width > 900 ? 3 : 2}
-                spaceBetween="16px"
-                centeredSlides
-              >
-                <SliderNavigation />
+          <div className="flex gap-4">
+            <Swiper
+              slidesPerView={width > 900 ? 3 : 2}
+              spaceBetween="16px"
+              centeredSlides
+            >
+              <SliderNavigation />
 
-                {MONTHS.map((month, index) => (
-                  <SwiperSlide key={month}>
-                    {({ isActive }) => (
-                      <SliderOption
-                        index={index}
-                        isActive={isActive}
-                        month={month}
-                      />
-                    )}
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </div>
-          </header>
-
-          <div className="flex flex-1 flex-col gap-2 overflow-y-auto pr-2">
-            <Transaction
-              amount={150}
-              date="05/12/2023"
-              title="Lunch"
-              type="expense"
-              category="food"
-            />
-            <Transaction
-              amount={12}
-              date="15/07/2023"
-              title="Salary"
-              type="income"
-            />
+              {MONTHS.map((month, index) => (
+                <SwiperSlide key={month}>
+                  {({ isActive }) => (
+                    <SliderOption
+                      index={index}
+                      isActive={isActive}
+                      month={month}
+                    />
+                  )}
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
-        </>
+        </header>
       )}
+
+      <div className="flex flex-1 flex-col gap-2 overflow-y-auto pr-2">
+        {(isLoading || !hasTransactions) && (
+          <>
+            <div className="flex h-full flex-col items-center justify-center gap-4">
+              {isLoading && <Spinner />}
+
+              {!isLoading && !hasTransactions && (
+                <>
+                  <img
+                    src={emptyStateImage}
+                    alt="No transactions found"
+                    className="w-40"
+                  />
+
+                  <p className="text-gray-700">
+                    We couldn&apos;t find any transactions!
+                  </p>
+                </>
+              )}
+            </div>
+          </>
+        )}
+
+        {!isLoading && !isInitialLoading && hasTransactions && (
+          <>
+            {transactions.map((transaction) => (
+              <Transaction
+                key={`${transaction.type}-${transaction.title}`}
+                amount={transaction.amount}
+                date={transaction.date}
+                title={transaction.title}
+                type={transaction.type as IconType}
+                category={transaction.category}
+              />
+            ))}
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
+++ src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
@@ -1,14 +1,29 @@
 import { useState } from "react";
 import { useDashboardContext } from "../../../../../app/hooks/useDashboard";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const transactions = [
+  {
+    amount: 150,
+    date: "05/12/2023",
+    title: "Lunch",
+    type: "expense",
+    category: "food",
+  },
+];
+
 export const useTransactionsController = () => {
   const { areValuesHidden } = useDashboardContext();
 
+  const [isInitialLoading, setIsInitialLoading] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   return {
     areValuesHidden,
+    isInitialLoading,
     isLoading,
     setIsLoading,
+    setIsInitialLoading,
+    transactions: [] as any[],
   };
 };
